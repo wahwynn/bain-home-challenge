8ce0b71a6206a7c396e2fffe7f9c8e4f80eda4df
Hoa Huynh <20475344+wahwynn@users.noreply.github.com>
2021-03-27 12:40:52 +0100

    - Add React Router - Scaffold basic workflow for the app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,12 +36,16 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
+  card: {
+    margin: 1,
+  },
 }));
 
 export default function CategoryPage() {
   const classes = useStyles();
 
-  const [books, setBooks] = useState({});
+  const [books, setBooks] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
     function fetchBooks() {
@@ -41,36 +55,85 @@ export default function CategoryPage() {
     }
     fetchBooks();
   }, []);
+  const handleAddToCart = book => {
+    setCartItems(prevCart => [...prevCart, book]);
+  };
+
+  const handleRemoveFromCart = cartId => {
+    console.log(cartId);
+    setCartItems(prevCart => {
+      prevCart.splice(cartId, 1);
+      return [...prevCart];
+    });
+  };
+
+  const handleClearAllCartItems = () => {
+    setCartItems([]);
+  };
+
+  const getCartItems = () => cartItems.length;
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Book Shop
-          </Typography>
-          <Button color="inherit">Cart</Button>
-        </Toolbar>
-      </AppBar>
-      <main className={classes.content}>
-        <BookCard books={books} />
-      </main>
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              component={Link}
+              to="/"
+            >
+              <HomeIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Book Shop
+            </Typography>
+            <Button color="inherit" component={Link} to="/cart">
+              <Badge badgeContent={getCartItems()} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/cart">
+            <ShoppingCart
+              cartItems={cartItems}
+              removeFromCart={handleRemoveFromCart}
+              clearAllCartItems={handleClearAllCartItems}
+            />
+          </Route>
+          <Route
+            path="/book/details/:bookId"
+            render={({ match }) => {
+              const bookId = match?.params?.bookId;
+              return (
+                <BookDetails
+                  book={books.find(book => book.Id === bookId)}
+                  addToCart={handleAddToCart}
+                />
+              );
+            }}
+          />
+          <Route path="/">
+            <main className={classes.content}>
+              <BookCard books={books} />
+            </main>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 const cardStyles = makeStyles({
   root: {
     minWidth: '50%',
-    marginBottom: 8,
+    margin: 8,
+    marginTop: 8,
   },
   title: {
     fontSize: 14,
@@ -82,30 +145,126 @@ const cardStyles = makeStyles({
 
 function BookCard(props) {
   const classes = cardStyles();
-
   const { books } = props;
 
   return (
-    <>
+    <Grid container spacing={3}>
       {Object.keys(books).map(bookIndex => {
         const book = books[bookIndex];
+        const bookId = book.Id;
 
         return (
-          <Card key={book.Id} className={classes.root}>
+          <Grid item key={bookId} xs={12} sm={4}>
+            <Card key={bookId} className={classes.root}>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  {book.Title}
+                </Typography>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {book.Author}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  component={Link}
+                  to={`/book/details/${bookId}`}
+                >
+                  Book in detail
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
+
+function BookDetails({ book, addToCart }) {
+  let { bookId } = useParams();
+  const classes = cardStyles();
+
+  if (!book) {
+    return (
+      <Card key={bookId} className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Not Found!
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+  return (
+    <Card key={bookId} className={classes.root}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {book.Title}
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {book.Author}
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {book.Genre} - {book.SubGenre} - {book.Height} - {book.Publisher}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          component={Link}
+          to="/"
+          onClick={() => addToCart(book)}
+        >
+          Add To Cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+function ShoppingCart({ cartItems, removeFromCart, clearAllCartItems }) {
+  const classes = cardStyles();
+  return (
+    <>
+      <Button size="small" onClick={clearAllCartItems}>
+        Clear All
+      </Button>
+      {Object.keys(cartItems).map(cartItemsIndex => {
+        const cartItem = cartItems[cartItemsIndex];
+        return (
+          <Card key={cartItem.Id} className={classes.root}>
             <CardContent>
               <Typography variant="h5" component="h2">
-                {book.Title}
+                {cartItem.Title}
               </Typography>
               <Typography
                 className={classes.title}
                 color="textSecondary"
                 gutterBottom
               >
-                {book.Author}
+                {cartItem.Author}
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Book in detail</Button>
+              <Button
+                size="small"
+                onClick={() => removeFromCart(cartItemsIndex)}
+              >
+                Remove
+              </Button>
             </CardActions>
           </Card>
         );
@@ -113,7 +272,27 @@ function BookCard(props) {
     </>
   );
 }
+const BookType = PropTypes.shape({
+  Id: PropTypes.string.isRequired,
+  Title: PropTypes.string.isRequired,
+  Author: PropTypes.string.isRequired,
+  Genre: PropTypes.string.isRequired,
+  SubGenre: PropTypes.string.isRequired,
+  Height: PropTypes.string.isRequired,
+  Publisher: PropTypes.string.isRequired,
+});
+
+BookDetails.propTypes = {
+  book: BookType,
+  addToCart: PropTypes.func.isRequired,
+};
 
 BookCard.propTypes = {
-  books: PropTypes.array.isRequired,
+  books: PropTypes.arrayOf(BookType).isRequired,
+};
+
+ShoppingCart.propTypes = {
+  cartItems: PropTypes.arrayOf(BookType).isRequired,
+  removeFromCart: PropTypes.func.isRequired,
+  clearAllCartItems: PropTypes.func.isRequired,
 };
